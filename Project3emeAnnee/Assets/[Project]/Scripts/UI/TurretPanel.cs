8fe07a44345beb3c117ceaed657cc5ee315aa2a5
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2024-09-26 12:54:58 +0200

    UI prefab and setup

---
+++ Project3emeAnnee/Assets/[Project]/Scripts/UI/TurretPanel.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class TurretPanel : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private RectTransform _panelRect;
+    [SerializeField] private float _showAnimationSpeed = 5;
+
+    private void Start()
+    {
+        ShowPanel(false, true);
+    }
+
+    private void ShowPanel(bool open, bool isInstante = false)
+    {
+        Vector3 startScale = _panelRect.localScale;
+        Vector3 targetScale = open ? Vector3.one : Vector3.zero;
+
+        if (isInstante)
+        {
+            _panelRect.localScale = targetScale;
+            return;
+        }
+
+        DOTween.To((time) =>
+        {
+            _panelRect.localScale = Vector3.Lerp(startScale, targetScale, time);
+        }, 0, 1, _showAnimationSpeed)
+        .SetSpeedBased()
+        .SetEase(Ease.Linear);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        print("Enter : " + name + " / " + transform.localScale);
+        ShowPanel(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        print("Exit : " + name + " / " + transform.localScale);
+        ShowPanel(false);
+    }
+}
