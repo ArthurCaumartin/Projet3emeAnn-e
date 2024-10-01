c2c33b87c67f9ef7c4373549234942db884c6cea
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2024-10-01 11:32:40 +0200

    Inventory link update

--- Project3emeAnnee/Assets/[Project]/Scripts/Turret/Inventory Link/TurretPartDescritor.cs
+++ Project3emeAnnee/Assets/[Project]/Scripts/Turret/Inventory Link/TurretPartDescritor.cs
@@ -1,10 +1,10 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TurretPartDescritor : MonoBehaviour
 {
     [SerializeField] private ScriptableTurretPart _turetPart;
-
     public List<ScriptableTurretPart> _turretPartList = new List<ScriptableTurretPart>();
 
     private void Start()
@@ -15,9 +15,10 @@ public class TurretPartDescritor : MonoBehaviour
     private void GenerateItem()
     {
         _turetPart = _turretPartList[Random.Range(0, _turretPartList.Count)];
+        GetComponent<Image>().sprite = _turetPart.inventorySprite;
     }
 
-    public ScriptableTurretPart GetItem()
+    public ScriptableTurretPart GetTurretPart()
     {
         return _turetPart;
     }
