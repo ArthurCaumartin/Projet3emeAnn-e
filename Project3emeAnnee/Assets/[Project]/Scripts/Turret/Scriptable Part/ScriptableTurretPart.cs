3e05d83c4afe7e8c5d0db963d78a5f34ada9e44c
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2024-10-01 11:32:05 +0200

    Add enum to turret part to compare them

--- Project3emeAnnee/Assets/[Project]/Scripts/Turret/Scriptable Part/ScriptableTurretPart.cs
+++ Project3emeAnnee/Assets/[Project]/Scripts/Turret/Scriptable Part/ScriptableTurretPart.cs
@@ -1,7 +1,18 @@
+using System;
 using UnityEngine;
+
+[Serializable]
+public enum PartType //TODO ALED enum pour verifier le type de part ? On n'a plusieur type mais on sais pas comment les comparer
+{
+    None,
+    Core,
+    Body,
+    Cannon,
+}
 
 public class ScriptableTurretPart : ScriptableObject
 {
+    public PartType partType;
     public string partName;
     [Space]
     public Sprite inventorySprite;
