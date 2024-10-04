c9bf3fec178295cb349e72afde09b54a07f87783
Dragomal <115795428+Dragomal@users.noreply.github.com>
2024-10-04 14:04:14 +0200

    fix (again) prefabs

--- Project3emeAnnee/Assets/[Project]/Scripts/Mob/SpawnSiegeMob.cs
+++ Project3emeAnnee/Assets/[Project]/Scripts/Mob/SpawnSiegeMob.cs
@@ -2,16 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class SpawnSiegeMob : MonoBehaviour
 {
     public List<Spawners> _spawners = new List<Spawners>();
+    public Button _startButton;
     private Transform _playerTransform;
 
     private void Start()
     {
         _playerTransform = PlayerInstance.instance.transform;
+        _startButton.onClick.AddListener(StartWaves);
     }
 
     public void StartWaves()
